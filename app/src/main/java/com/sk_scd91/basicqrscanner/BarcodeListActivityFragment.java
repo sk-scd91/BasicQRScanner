7fd016f646f8b04d02d48ff63c42ab64770cd44a
sk-scd91 <scdeneen@gmail.com>
2017-01-18 16:40:46 -0500

    Initial Commit

---
+++ app/src/main/java/com/sk_scd91/basicqrscanner/BarcodeListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.sk_scd91.basicqrscanner;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BarcodeListActivityFragment extends Fragment {
+
+    public BarcodeListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_barcode_list, container, false);
+    }
+}
