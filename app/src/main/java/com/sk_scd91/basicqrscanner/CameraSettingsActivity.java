758797038f6cc05931539b586ae9829a50fccdf3
sk-scd91 <scdeneen@gmail.com>
2017-07-24 13:38:47 -0400

    Use Support Toolbar for Camera Settings

--- app/src/main/java/com/sk_scd91/basicqrscanner/CameraSettingsActivity.java
+++ app/src/main/java/com/sk_scd91/basicqrscanner/CameraSettingsActivity.java
@@ -4,6 +4,7 @@ import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CameraSettingsActivity extends AppCompatActivity {
 
@@ -11,6 +12,12 @@ public class CameraSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         getFragmentManager().beginTransaction()
                 .replace(R.id.camera_settings_view, new CameraSettingsFragment())
